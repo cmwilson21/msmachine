d7230cd6dfb81726a8acb3c46c9ed18530ff48b3
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-08 18:32:15 -0600

    added a card for machining

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      Tell us something
+      <h3>Reach out and contact us</h3>
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,11 +7,11 @@ const NavBar = () => {
       <ul className="nav-ul">
         <li className="nav-li"><NavLink className="navbar_link" to="/">Home</NavLink></li>
         <li className="nav-li"><NavLink className="navbar_link" to="/about">About</NavLink></li>
-        <li className="nav-li"><NavLink className="navbar_link" to="/contact">Contact</NavLink></li>
         <li className="nav-li"><NavLink className="navbar_link" to="/machining">Machinging</NavLink></li>
         <li className="nav-li"><NavLink className="navbar_link" to="fabrication">Fabrication</NavLink></li>
         <li className="nav-li"><NavLink className="navbar_link" to="assembly">Assembly</NavLink></li>
         <li className="nav-li"><NavLink className="navbar_link" to="quality">Quality</NavLink></li>
+        <li className="nav-li"><NavLink className="navbar_link" to="/contact">Contact</NavLink></li>
         <li className="nav-li"><NavLink className="navbar_link" to="careers">Careers</NavLink></li>
       </ul>
     </div>
