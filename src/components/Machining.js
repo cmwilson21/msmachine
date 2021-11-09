21c0447a89ea300b89217c20a9879ce0421fd239
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-08 19:40:00 -0600

    added cards to machining

--- src/components/Machining.js
+++ src/components/Machining.js
@@ -4,29 +4,95 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import Grid from '@mui/material/Grid';
+// import Item from '@mui/material/Item';
+import { styled } from '@mui/material/styles'
+import Paper from '@mui/material/Paper';
 
 
 const Machining = () => {
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
   return (
-    <Card sx={{ maxWidth: 500 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Grid container spacing={2}>
+      <Grid item xs={6} md={4}>
+        <Item>
+          <Card sx={{ maxWidth: 500 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image="https://images.unsplash.com/photo-1599728613885-bc908f9d6d04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                alt="cnc machine"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Item>
+      </Grid>
+
+      <Grid item xs={6} md={4}>
+        <Item>
+
+          <Card sx={{ maxWidth: 500 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image="https://images.unsplash.com/photo-1599728613885-bc908f9d6d04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                alt="cnc machine"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Item>
+      </Grid>
+      <Grid item xs={6} md={4}>
+        <Item>
+
+          <Card sx={{ maxWidth: 500 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image="https://images.unsplash.com/photo-1599728613885-bc908f9d6d04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                alt="cnc machine"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Item>
+      </Grid>
+    </Grid>
   );
 }
 
