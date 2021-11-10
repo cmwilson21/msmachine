1a153cd20696f87a9b040b7b177d5d9dbe186e0e
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-09 18:42:52 -0600

    added logo to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Typography } from '@mui/material'
 
 const Home = () => {
   return (
     <div>
-      Take me home!
+      {/* <img src="https://ibb.co/0JZ53yg" alt="msm logo" /> */}
+      <img src="https://i.ibb.co/bdLtn5S/Final-Transparent-Copy.png" alt="Final-Transparent-Copy" border="0" className="msm-logo"/>
+      <Typography style={{fontSize: '30px'}}>For all of your precision machining and high quality fabrication needs.</Typography>
     </div>
   )
 }
